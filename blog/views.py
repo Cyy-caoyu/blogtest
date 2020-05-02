e3ce2d7d275bd57ab4a7f01e56b26cac5055b94a
your name <your email>
2020-05-02 10:17:00 +0800

    index类视图

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,13 @@ from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+from django.views.generic import ListView
 
 
-def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
